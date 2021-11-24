3275ccc1cdd344a88499908ce458be92b4511d08
Abdul Masjit <abdulmasjits@gmail.com>
2021-11-24 08:34:02 +0700

    First commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var firstname string = "John"
+	var lastname string
+	lastname = "wick"
+
+	nickname := "Nabila"
+
+	fmt.Println("Hello World")
+	fmt.Printf("Halo %s %s!\n", firstname, lastname)
+	fmt.Println("Nickname " + nickname)
+}
